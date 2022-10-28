721cf4b5916fd91adeaf3be4559c91b30d189d5b
jacoposergio <djago32@gmail.com>
2022-10-28 15:31:12 +0200

    esercizio 5

--- Program.cs
+++ Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//void StampaArray(int[] array): che preso un array di numeri interi, 
+//    stampa a video il contenuto dell’array in questa forma
+//    “[elemento 1, elemento 2, elemento 3, ...]“.
+
+//int[] arrayTest = { 1, 2, 3, 4, 5 };
+//StampaArray(arrayTest);
+
+//void StampaArray(int[] array)
+//{
+//    Console.Write("[");
+//    for (int i = 0; i < array.Length; i++)
+//    {
+//        Console.Write("elemento" + array[i] + ", " );
+//    }
+//    Console.Write("]");
+//}
+
+// *****fare l'ultimo elemento dell'array con l'if ******
+
+
+//********************************************************************************************************
+
+//int Quadrato(int numero): che vi restituisca il quadrato del numero passato come parametro. 
+
+
+//int Quadrato(int likeToSquare)
+//{
+//   return likeToSquare * likeToSquare;
+//}
+
+//Console.WriteLine("Inserisci un numero");
+//int userNumber = Convert.ToInt32(Console.ReadLine());
+//int userNumberSquare = Quadrato(userNumber);
+
+//Console.WriteLine("numero al quadrato: " + userNumberSquare);
+
+
+//********************************************************************************************************
+
+//int[] ElevaArrayAlQuadrato(int[] array): che preso un array di numeri interi, restituisca un nuovo array con tutti
+//    gli elementi elevati quadrato. Attenzione: è importante restituire un nuovo array, e non modificare l’array come 
+//    parametro della funzione! Vi ricordate perchè? Pensateci (vedi slide)
+
+
+int ElevaArrayAlQuadrato(int[] array)
+{
+    int[] arraySquared = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        arraySquared[i] = array[i] * array[i];
+    }
+    return arraySquared;
+}
+
+int[] arrayTest = { 1, 2, 3, 4, 5 };
+int[] arrayTestSquared = ElevaArrayAlQuadrato(arrayTest);
+
+
+
+
