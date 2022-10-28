aac85af4418c3fdacb695f9b5cd6c2465fd43612
jacoposergio <djago32@gmail.com>
2022-10-28 17:27:31 +0200

    inizio fattoriale

--- Program.cs
+++ Program.cs
@@ -1,61 +1,73 @@
-﻿//Funzioni
+﻿////Funzioni
 
-void StampaArray(int[] array)
+//void StampaArray(int[] array)
+//{
+//    Console.Write("[");
+//    for (int i = 0; i < array.Length; i++)
+//    {
+//        if(i < array.Length - 1)
+//        Console.Write("elemento " + array[i] + ", ");
+//        else
+//            Console.Write("elemento " + array[i] );
+//    }
+//    Console.Write("]");
+//}
+
+//int Quadrato(int likeToSquare)
+//{
+//    return likeToSquare * likeToSquare;
+//}
+
+
+//int[] ElevaArrayAlQuadrato(int[] array)
+//{
+//    int[] arraySquared = new int[array.Length];
+
+//    for (int i = 0; i < array.Length; i++)
+//    {
+//        arraySquared[i] = Quadrato(array[i]);
+//    }
+//    return arraySquared;
+//}
+
+
+//int SommaElementiArray(int[] array)
+//{
+//    int sommaTotaleElementi = 0;
+//    for (int i = 0; i < array.Length; i++)
+//    {
+//        sommaTotaleElementi += array[i];
+//    }
+//    return sommaTotaleElementi;
+//}
+
+////Applicazione funzioni
+
+//int[] arrayTest = { 2, 6, 7, 5, 3, 9 };
+
+//StampaArray(arrayTest);
+//Console.WriteLine("\r\n");
+//int[] arrayAlQuadrato = ElevaArrayAlQuadrato(arrayTest);
+//StampaArray(arrayAlQuadrato);
+//Console.WriteLine("\r\n");
+//StampaArray(arrayTest);
+//Console.WriteLine("\r\n");
+//int sommaTuttiNumeri = SommaElementiArray(arrayTest);
+//Console.WriteLine("La somma di tutti i numeri è " + sommaTuttiNumeri);
+//Console.WriteLine("\r\n");
+//Console.WriteLine("La somma di tutti i numeri al quadrato è " + Quadrato(sommaTuttiNumeri));
+
+
+int CalcoloFattoriale(int numero)
 {
-    Console.Write("[");
-    for (int i = 0; i < array.Length; i++)
+    for (int i = numero - 1; i > 0; i--)
     {
-        if(i < array.Length - 1)
-        Console.Write("elemento " + array[i] + ", ");
-        else
-            Console.Write("elemento " + array[i] );
+        numero *= i;
     }
-    Console.Write("]");
+    return numero;
 }
 
-int Quadrato(int likeToSquare)
-{
-    return likeToSquare * likeToSquare;
-}
-
-
-int[] ElevaArrayAlQuadrato(int[] array)
-{
-    int[] arraySquared = new int[array.Length];
-
-    for (int i = 0; i < array.Length; i++)
-    {
-        arraySquared[i] = Quadrato(array[i]);
-    }
-    return arraySquared;
-}
-
-
-int SommaElementiArray(int[] array)
-{
-    int sommaTotaleElementi = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        sommaTotaleElementi += array[i];
-    }
-    return sommaTotaleElementi;
-}
-
-//Applicazione funzioni
-
-int[] arrayTest = { 2, 6, 7, 5, 3, 9 };
-
-StampaArray(arrayTest);
-Console.WriteLine("\r\n");
-int[] arrayAlQuadrato = ElevaArrayAlQuadrato(arrayTest);
-StampaArray(arrayAlQuadrato);
-Console.WriteLine("\r\n");
-StampaArray(arrayTest);
-Console.WriteLine("\r\n");
-int sommaTuttiNumeri = SommaElementiArray(arrayTest);
-Console.WriteLine("La somma di tutti i numeri è " + sommaTuttiNumeri);
-Console.WriteLine("\r\n");
-Console.WriteLine("La somma di tutti i numeri al quadrato è " + Quadrato(sommaTuttiNumeri));
-
+int numberTest = 4;
+Console.WriteLine("Il risultato del fattoriale è: " + CalcoloFattoriale(numberTest));
 
 
